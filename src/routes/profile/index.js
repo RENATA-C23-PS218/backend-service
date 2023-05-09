e5bedb1292563dae82988577c62902917a5e2f93
Rico <rico@cakeplabs.com>
2023-05-10 00:32:30 +0800

    RD-31 update: add routes for profile API service

---
+++ src/routes/profile/index.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const controllers = require("../../controllers/profile");
+const validation = require("../../utils/profile/validation");
+const middlewares = require("../../middlewares/restrict");
+
+router.post(
+  "/profile",
+  middlewares.restrict,
+  validation.createUserProfileValidation,
+  controllers.create
+);
+router.get("/profile", middlewares.restrict, controllers.get);
+
+module.exports = router;
