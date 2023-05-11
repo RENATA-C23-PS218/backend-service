20dbe96045de8d20f211d64e1d3155f1b444ca4b
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-05-11 10:30:33 +0800

    RD-31 add route for upload profile image

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -2,6 +2,13 @@ const router = require("express").Router();
 const controllers = require("../../controllers/profile");
 const validation = require("../../utils/profile/validation");
 const middlewares = require("../../middlewares/restrict");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // keep images size < 5 MB
+  },
+});
 
 router.post(
   "/profile",
@@ -10,5 +17,11 @@ router.post(
   controllers.update
 );
 router.get("/profile", middlewares.restrict, controllers.get);
+router.post(
+  "/profile-image",
+  middlewares.restrict,
+  upload.single("file"),
+  controllers.upAvatar
+);
 
 module.exports = router;
