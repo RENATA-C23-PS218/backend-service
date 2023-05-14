b5ed0dd039b74897ef1e8ee4954232f609dc56c1
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-05-15 03:51:32 +0800

    RD-65 Swagger API and .env PATH

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const auth = require("./auth");
 const profile = require("./profile");
+const apiDocs = require("./api-docs");
 
 router.use(auth);
 router.use(profile);
+router.use(apiDocs);
 
 module.exports = router;
