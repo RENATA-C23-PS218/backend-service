864d4298179c65a3319b9cbe983e3efb0b48bc3c
Wayan Deden Setyawan <dedensetyawan17120104@gmail.com>
2023-05-23 18:39:55 +0800

    add new models and migrations with their relation

---
+++ src/models/plant.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Plant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Plant.belongsTo(models.Soil, {
+        foreignKey: "soil_id",
+        as: "soil",
+      });
+    }
+  }
+  Plant.init(
+    {
+      soil_id: DataTypes.STRING,
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Plant",
+    }
+  );
+  return Plant;
+};
